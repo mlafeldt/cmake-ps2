3afd538103dc9475164ce933453383499efd30a9
Mathias Lafeldt <misfire@debugon.org>
2010-06-14 15:13:27 +0200

    Replace cube sample code with hello world.

---
+++ hello.c
@@ -0,0 +1,24 @@
+#include <tamtypes.h>
+#include <sifcmd.h>
+#include <kernel.h>
+#include <sifrpc.h>
+#include <stdio.h>
+#include <debug.h>
+#include "sio.h"
+
+int main()
+{   
+   SifInitRpc(0); 
+
+   init_scr();
+   scr_printf("Hello, world!\n"); // hello world in the screen
+#if 0
+   printf("Hello, world!\n");
+   nprintf("Hello, again, from Naplink RPC!\n");
+   
+   sio_init(115200, 0, 0, 0, 0);
+   sio_printf("Hello from EE SIO!\n");
+#endif
+   /* Return to the bootloader or PS2 browser. */
+   return 0;
+}
