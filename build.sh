35196c4348ea6f283479689c45737711ce3818cd
Mathias Lafeldt <misfire@debugon.org>
2010-06-16 09:46:43 +0200

    Move hello sample to separate folder.

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-cd build/ || exit 1
-cmake -DCMAKE_VERBOSE_MAKEFILE=TRUE -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchain-ee.cmake .. || exit 1
+dir=${1:?"source directory missing"}
+
+cd build || exit 1
+rm -rf *
+cmake -DCMAKE_VERBOSE_MAKEFILE=TRUE -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchain-ee.cmake "../${dir}" || exit 1
 make
