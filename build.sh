1eb167479b5e17c5e2f8063524d962cf6ba6938f
Mathias Lafeldt <misfire@debugon.org>
2010-06-25 11:51:47 +0200

    Add platform file for IOP.

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-dir=${1:?"source directory missing"}
+arch=${1:?"arch missing"}
+dir=${2:?"source directory missing"}
 
 cd build || exit 1
 rm -rf *
-cmake -DCMAKE_VERBOSE_MAKEFILE=TRUE -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchain-ee.cmake "../${dir}" || exit 1
+cmake -DCMAKE_VERBOSE_MAKEFILE=TRUE \
+    -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchain-${arch}.cmake \
+    "../${dir}" || exit 1
 make
