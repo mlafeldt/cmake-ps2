52a71ef4f8322e4f8dda76049aa33aca55deadc6
Mathias Lafeldt <misfire@debugon.org>
2010-07-01 15:14:50 +0200

    Don't set CMAKE_VERBOSE_MAKEFILE. Use make VERBOSE=1 instead.

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ dir=${2:?"source directory missing"}
 
 cd build || exit 1
 rm -rf *
-cmake -DCMAKE_VERBOSE_MAKEFILE=TRUE \
-    -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchain-${arch}.cmake \
+cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchain-${arch}.cmake \
     "../${dir}" || exit 1
 make
